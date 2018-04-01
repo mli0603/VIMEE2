dafe8fda333d8f20ffb7a165efb6f0914898d092
klynx <KevinLynx_L@hotmail.com>
2018-03-31 18:02:03 -0700

    Accelerometer calculation changed

--- accelerometer/accelerometer.ino
+++ accelerometer/accelerometer.ino
@@ -1,5 +1,6 @@
 #include <math.h>
 
+// scaling and zero values
 const int YZERO1 = 340;
 const int NEGONEG1 = 272;
 const int ONEG1 = 409;                 // acc1
@@ -9,9 +10,11 @@ const int NEGONEG2 = 281;
 const int ONEG2 = 417;                 // acc2
 
 const int XACC1 = A0;                  // acc1: x-axis of the accelerometer
-const int YACC1 = A1;                  // acc1: y-axis of the accelerometer
+const int YACC1 = A1;                  // acc1: y-axis of the accelerometer (actually now z axis for fwd accelerometer)
 const int XACC2 = A2;                  // acc2: x-axis of the accelerometer
 const int YACC2 = A3;                  // acc2: y-axis of the accelerometer
+
+double acc_scale = 3.3/5.0*1023;
 
 float xacc1;
 float yacc1;
@@ -43,26 +46,45 @@ void loop() {
   Serial.print(yacc2);
   Serial.print("\t");
 
-  angle1 = acos(toAcc(xacc1,NEGONEG1,ONEG1));
-  if (yacc1 < YZERO1)
+//  angle1 = acos(toAcc(xacc1,NEGONEG1,ONEG1));
+  angle1 = toAcc(xacc1,YZERO1, NEGONEG1,ONEG1);
+//  if (yacc1 < YZERO1)
     Serial.print(angle1);
-  else
-    Serial.print(-angle1);
+//  else
+//    Serial.print(-angle1);
   Serial.print("\t");
   
-  angle2 = acos(toAcc(xacc2,NEGONEG2,ONEG2));
-  if (yacc2 < YZERO2)
+  angle2 = acos(toAcc(xacc2, YZERO2 ,NEGONEG2,ONEG2));
+//  if (yacc2 < YZERO2)
     Serial.print(angle2);
-  else
-    Serial.print(-angle2);
-    
+//  else
+//    Serial.print(-angle2);
+
+//  Serial.print(toDeg(acc_scale(XACC1), acc_scale(YACC1)));
+//  Serial.print("\t");
+//  Serial.print(toDeg(acc_scale(XACC2), acc_scale(YACC2)));
+
   Serial.println();
   // delay before next reading:
   delay(20);
 }
 
-float toAcc(int value, int negative, int positive) {
+float toAcc(float value, float zero, float negative, float positive) {
   // convert digital value to acceleration in g
   return 2.0*(value-negative)/(positive-negative)-1;
 }
+
+float acc_scale(float value, float zero, float negative, float positive){
+  float temp = (value-zero)/(positive-negative) * 1023.0;
+  
+
+  
+  return temp;
+}
+
+// converts to roll angle in rads
+float toAngle(int downaxis, int axis2){  
+  return atan2(downaxis,axis2);
+}
+
 
